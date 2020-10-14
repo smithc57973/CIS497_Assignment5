b80b84941cae1a04919d258db2848570172e2f68
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-10-14 16:35:14 -0500

    Added Unity 2020 version

---
+++ Assignment5_3D_2020/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Chris Smith
+ * Assignment53D
+ * Controls player movement 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Reference to character controller
+    public CharacterController controller;
+    public GameManager gameManager;
+    public UIManager uiManager;
+    //Player move speed
+    public float speed = 12f;
+    //Gravity
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public float gravityMultiplier = 2f;
+    //Ground Check
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+    //Player jump height
+    public float jumpHeight = 3f;
+
+
+    private void Awake()
+    {
+        //Reset gravity based on multiplier
+        gravity *= gravityMultiplier;
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameManager.gameOver)
+        {
+            //Checking if player is on the ground
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            //Reset gravity upon landing
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            //Get movement input
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            //Create movement vector
+            Vector3 move = transform.right * x + transform.forward * z;
+            //Apply move
+            controller.Move(move * speed * Time.deltaTime);
+            //Jumping
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+            //Add gravity to velocity
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            gameManager.coins++;
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            gameManager.gameOver = true;
+            uiManager.winText.enabled = true;
+        }
+    }
+}
---
+++ Assignment5_3D_2020/Assets/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Chris Smith
+ * Assignment53D
+ * Controls target health
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameManager gameManager;
+    //Targetable objects health
+    public float health = 50f;
+
+    private void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("Player").GetComponent<GameManager>();
+    }
+
+    //On taking damage
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    //On death
+    void Die()
+    {
+        Destroy(gameObject);
+        gameManager.kills++;
+    }
+}
